81f43cc6829289fec9ffa85e9de23823e189f758
dekud <dekud@mail.ru>
2015-10-13 01:01:48 +0300

    post

--- nodejs/requestHandlers.js
+++ nodejs/requestHandlers.js
@@ -1,7 +1,7 @@
 var exec = require("child_process").exec;
+var querystring = require("querystring");
 
-
-function start(response) {
+function start(response, postData) {
 	console.log("Request handler 'start' was called");
 
 //	exec("ls -lah", function (error,stdout,stderr) {
@@ -24,10 +24,10 @@ function start(response) {
 
 }
 
-function upload(response) {
+function upload(response, postData) {
 	console.log("Request handler 'upload' was called");
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello Upload");
+	response.write("You've sent the text: " + querystring.parse(postData).text);
 	response.end();
 
 }
--- nodejs/server.js
+++ nodejs/server.js
@@ -4,9 +4,20 @@ var url = require("url");
 function start(route, handle) {
 	function onRequest(req,res) {
 		var pathname = url.parse(req.url).pathname;
-		console.log("Request for " + pathname + " received." );
+		var postData = "";
+                console.log("Request for " + pathname + " received." );
+                
+                req.setEncoding("utf8");
+                
+                req.addListener("data", function(postDataChunk) {
+                    postData += postDataChunk;
+                    console.log("Received POST data chunk '"+
+                    postDataChunk + "'.");
+                });
 
-		route(handle, pathname, res);
+                req.addListener("end", function() {
+                    route(handle, pathname, res, postData);
+                });
 	}
 
 
