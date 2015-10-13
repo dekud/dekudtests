0e8a3cebf1d34d5b2e3d11f4a6706192abe3d667
dekud <dekud@localhost.localdomain>
2015-10-13 13:30:58 +0300

    full examles

--- nodejs/server.js
+++ nodejs/server.js
@@ -4,20 +4,8 @@ var url = require("url");
 function start(route, handle) {
 	function onRequest(req,res) {
 		var pathname = url.parse(req.url).pathname;
-		var postData = "";
                 console.log("Request for " + pathname + " received." );
-                
-                req.setEncoding("utf8");
-                
-                req.addListener("data", function(postDataChunk) {
-                    postData += postDataChunk;
-                    console.log("Received POST data chunk '"+
-                    postDataChunk + "'.");
-                });
-
-                req.addListener("end", function() {
-                    route(handle, pathname, res, postData);
-                });
+		route(handle, pathname, res, req);
 	}
 
 
