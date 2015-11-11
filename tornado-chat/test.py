66d1058a9152035b06678b578cc33cb59787d779
dekud <dekud@dekud-VirtualBox.(none)>
2015-11-11 17:35:26 +0400

    simple test jot

---
+++ tornado-chat/test.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import tornado.ioloop
+import tornado.web
+
+class Application(tornado.web.Application):
+	def __init__(self):
+		handlers = [
+			(r'/',MainHandler),
+			(r'/test',TestHandler),
+		]
+
+		tornado.web.Application.__init__(self,handlers)
+
+
+class MainHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.write("Hello, world")
+	def data_received(self, chunk):
+		print("chunk")
+
+class TestHandler(tornado.web.RequestHandler):
+	def __init__(self, application, request, **kwargs):
+		tornado.web.RequestHandler.__init__(self, application, request, **kwargs)
+		self.stream = None
+
+	@tornado.web.asynchronous
+	def get(self):
+		print("La-La-La")
+		self.stream = self.request.connection.detach()
+		print("---1---")
+		self.stream.set_close_callback(self.on_connection_close)
+		print("---2---")
+		try:
+			self.stream.write(tornado.escape.utf8(
+				"HTTP/1.1 101 Switching Protocols\r\n"
+				"Upgrade: test\r\n"
+				"Connection: Upgrade\r\n"
+			"\r\n"))
+			
+			self._receive_frame()
+
+		except ValueError:
+			print(ValueError.__dict__)
+			self._abort()
+		except:
+			print("Error")
+
+		print("---3---")
+	
+	def on_connection_close(self):
+		print("close")
+		pass
+	
+	def _receive_frame(self):
+		try:
+			self.stream.read_bytes(2, self._on_frame_start)
+		except StreamClosedError:
+			self._abort()
+
+	def _on_frame_start(self,data):
+		print(data)
+		self.stream.write(tornado.escape.utf8("""{"req": "PING", "v": 1, "req_id": "0x0666", "arg": {}}"""))
+		self.stream.read_bytes(10,self._on_frame_start)
+		pass
+
+	def _abort(self):
+		self.stream.close()
+
+def make_app():
+	return tornado.web.Application([ (r"/", MainHandler), ])
+
+if __name__ == "__main__":
+	app = Application()
+	app.listen(8888)
+	tornado.ioloop.IOLoop.current().start()
