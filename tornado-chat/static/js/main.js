66ab4882ec7fffe18f16a7df2d268333edf335aa
dekud <dekud@dekud-VirtualBox.(none)>
2015-11-25 16:49:29 +0400

    add files

---
+++ tornado-chat/static/js/main.js
@@ -0,0 +1,18 @@
+var lastId = -1;
+function update_messages()
+{
+	$.ajax({
+    url: '/messages?last='+lastId,       
+    dataType : "json",                   
+    success: function (data, textStatus) { 
+        $.each(data, function(i, d)
+		{    
+			$("#table_event").append("<tr><td>"+d['user']+"</td><td>"+d['text']+"</td><td>"+d['datetime']+"</td></tr>");
+			lastId = d['id']
+        });
+		$("#div_body")[0].scrollTop = $("#div_body")[0].scrollHeight;
+    } 
+	});
+	setTimeout(function(){ update_messages()},10000);
+}
+
